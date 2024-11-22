454edeae26d27d921955d41a75ff6ac660dc1199
misuaa <nurulinayahh19@student.ub.ac.id>
2024-11-22 19:56:22 +0700

    asd
    sd

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameOverScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void Retty(){
+    SceneManager.LoadScene("Retry");
+   }
+   public void Exit(){
+    SceneManager.LoadScene("Exit");
+   }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,16 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
